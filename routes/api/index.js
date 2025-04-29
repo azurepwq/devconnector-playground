8577b8f1183b0052e2e47acf8b4c086a1d527a5c
azurepwq <azurepwq@gmail.com>
2025-04-29 22:04:20 +0800

    add routes: posts, users, profile

---
+++ routes/api/index.js
@@ -0,0 +1,9 @@
+const profile = require("./posts");
+const users = require("./users");
+const posts = require("./posts");
+
+module.exports = {
+  users,
+  profile, 
+  posts
+}
--- server.js
+++ server.js
@@ -1,10 +1,28 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 
 const app = express();
+
+// MongoDB config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose.
+  connect(db).
+  then(() => console.log('MongoDB Connected')).
+  catch(err => console.log(err));
 
 app.get('/', (req, res) => {
   return res.send("Hello, azurepwq!");
 });
+
+// Use Routes
+const { profile, posts, users } = require('./routes/api');
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
 
 const port = process.env.PORT || 3000;
 
